1c3bb6ae3660fa019808ccf60d77845396268b97
unknown <akinwumiadekanmi2@gmail.com>
2023-04-18 13:47:38 +0100

    another commit

--- router/year-router.js
+++ router/year-router.js
@@ -5,16 +5,10 @@ const dayController = require("./../controller/days-controller");
 
 const router = express.Router();
 
-router
-  .route("/years")
-  // .post(yearController.addYear)
-  .get(yearController.getAllYears);
+router.route("/years").get(yearController.getAllYears);
 
-router
-  .route("/months")
-  // .post(monthController.addMonth)
-  .get(monthController.getAllMonths);
+router.route("/months").get(monthController.getAllMonths);
 
-router.route("/days").post(dayController.addDay).get(dayController.getAllDays);
+router.route("/days").get(dayController.getAllDays);
 
 module.exports = router;
